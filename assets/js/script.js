2fef4c776d5510aae1ff7d90408b338ac586e09a
vishnududi-511 <dudivishnu4@gmail.com>
2024-03-16 09:50:12 +0530

    sec-6 done

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,34 @@ let menubox = document.querySelector('.menubox');
 menubox.addEventListener("click", function () {
     navitems.classList.toggle("show")
 })
+
+
+$('.slider-1').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 2,
+    slidesToScroll: 1,
+  autoplay:false,
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 780,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
