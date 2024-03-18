f73c5b51be5c57d1351e162fd1a372d819476432
vishnududi-511 <dudivishnu4@gmail.com>
2024-03-18 14:35:57 +0530

    footer svg adjusted, roots added, slider-1 updated and sec-10 img fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,7 +24,7 @@ $('.slider-1').slick({
       }
     },
     {
-      breakpoint: 780,
+      breakpoint: 750,
       settings: {
         slidesToShow: 1,
         slidesToScroll: 1
@@ -54,7 +54,7 @@ $('.slider-2').slick({
       }
     },
     {
-      breakpoint: 740,
+      breakpoint: 750,
       settings: {
         slidesToShow: 1,
         slidesToScroll: 1
